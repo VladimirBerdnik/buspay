51ca5aa1c8f1704ea604cd5a644c706c1d044f83
Vladimir Berdnik <vladimir.a.berdnik@gmail.com>
2018-10-28 15:43:56 +0600

    Improve application configuration

--- config/buspay.php
+++ config/buspay.php
@@ -1,22 +1,24 @@
 <?php
 
+use App\Domain\Enums\CardTypes;
+
 return [
     /**
      * Fare configuration.
      */
     'fare' => [
         // Base payment card type slug
-        'base_card_type_slug' => null,
+        'base_card_type_slug' => CardTypes::DEFAULT,
 
         // Preferential card types slugs, used to determine compensation amount to transport companies
-        'preferential_card_types' => [],
+        'preferential_card_types' => [CardTypes::CHILD, CardTypes::RETIRE, CardTypes::REDUCED, CardTypes::FREE],
     ],
     /**
      * Driver related configuration.
      */
     'driver' => [
         // Drivers card type identifier
-        'card_type_slug' => null,
+        'card_type_slug' => CardTypes::DRIVER,
 
         // Hour at which driver's shift on bus should be automatically cancelled (0-23)
         'shift_cancel_hour' => 5,
