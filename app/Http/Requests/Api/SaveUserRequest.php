eaaf75094f06b0c2ee7408abc2137b76e6566ed3
Vladimir Berdnik <vladimir.a.berdnik@gmail.com>
2018-11-04 14:05:17 +0600

    Resolve issue with user request validation rules

--- app/Http/Requests/Api/SaveUserRequest.php
+++ app/Http/Requests/Api/SaveUserRequest.php
@@ -7,10 +7,12 @@ use App\Domain\Enums\RolesIdentifiers;
 use App\Models\User;
 use Saritasa\Laravel\Validation\GenericRuleSet;
 use Saritasa\Laravel\Validation\Rule;
+use Saritasa\Laravel\Validation\RuleSet;
 
 /**
  * SaveUserRequest form request.
  *
+ * @property-read integer $id User identifier
  * @property-read integer $role_id User role identifier
  * @property-read integer|null $company_id Company identifier in which user works
  * @property-read string $first_name User first name
@@ -29,19 +31,21 @@ class SaveUserRequest extends ApiRequest
     {
         return [
             User::ROLE_ID => Rule::required()->exists('roles', 'id')->int(),
-            User::COMPANY_ID => Rule::int()->when(
+            User::COMPANY_ID => Rule::exists('companies', 'id')->int()->when(
                 in_array($this->role_id, [RolesIdentifiers::OPERATOR]),
-                function (GenericRuleSet $rules) {
-                    return $rules->required()->exists('companies', 'id');
+                function (RuleSet $rules) {
+                    return $rules->required();
                 },
-                function (GenericRuleSet $rules) {
+                function (RuleSet $rules) {
                     return $rules->nullable();
                 }
             ),
             User::FIRST_NAME => Rule::required()->string()->max(191),
             User::LAST_NAME => Rule::required()->string()->max(191),
             User::EMAIL => Rule::required()->string()->max(191),
-            User::PASSWORD => Rule::required()->string()->max(191),
+            User::PASSWORD => Rule::string()->max(191)->when(!$this->id, function (RuleSet $rules) {
+                return $rules->required();
+            }),
         ];
     }
 
