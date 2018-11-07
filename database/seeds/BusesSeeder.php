7aee44f9ab47e520487cb0f6ab4895bdf778cf6b
Vladimir Berdnik <vladimir.a.berdnik@gmail.com>
2018-11-07 23:27:29 +0600

    Improve seeders logic

--- database/seeds/BusesSeeder.php
+++ database/seeds/BusesSeeder.php
@@ -17,9 +17,9 @@ class BusesSeeder extends Seeder
             factory(Bus::class, random_int(10, 40))
                 ->make([Bus::COMPANY_ID => $company->getKey()])
                 ->each(function (Bus $bus) use ($company) {
-                    if (random_int(0, 3)) {
-                        $bus->route_id = $company->routes()->inRandomOrder()->firstOrNew([])->getKey();
-                    }
+                    $bus->route_id = random_int(0, 3)
+                        ? $company->routes()->inRandomOrder()->firstOrNew([])->getKey()
+                        : null;
 
                     $bus->save();
                 });
--- database/seeds/DriversSeeder.php
+++ database/seeds/DriversSeeder.php
@@ -14,10 +14,12 @@ class DriversSeeder extends Seeder
     public function run()
     {
         Company::query()->get()->each(function (Company $company) {
-            factory(Driver::class, random_int(20, 50))->create([
-                Driver::COMPANY_ID => $company->getKey(),
-                Driver::BUS_ID => random_int(0, 3) ? $company->buses()->inRandomOrder()->first()->getKey() : null,
-            ]);
+            factory(Driver::class, random_int(20, 50))->make([])
+                ->each(function (Driver $driver) use ($company) {
+                    $driver->bus_id = random_int(0, 6) ? $company->buses()->inRandomOrder()->first()->getKey() : null;
+                    $driver->company_id = $company->getKey();
+                    $driver->save();
+                });
         });
     }
 }
