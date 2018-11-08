e54a2bb1af4dff9ac2b6ee73ba8d0ce48d8f1c29
Vladimir Berdnik <vladimir.a.berdnik@gmail.com>
2018-11-08 23:56:21 +0600

    Improve driver service CRUD methods

--- app/Domain/Services/DriverService.php
+++ app/Domain/Services/DriverService.php
@@ -2,11 +2,224 @@
 
 namespace App\Domain\Services;
 
+use App\Domain\Dto\DriverData;
+use App\Domain\Enums\CardTypesIdentifiers;
+use App\Domain\Exceptions\Constraint\DriverDeletionException;
+use App\Domain\Exceptions\Constraint\DriverReassignException;
+use App\Domain\Exceptions\Integrity\UnexpectedCardForDriverException;
 use App\Extensions\EntityService;
+use App\Models\Bus;
+use App\Models\Card;
+use App\Models\Company;
+use App\Models\Driver;
+use Carbon\Carbon;
+use Illuminate\Database\ConnectionInterface;
+use Illuminate\Validation\Rules\Exists;
+use Illuminate\Validation\Rules\Unique;
+use Illuminate\Validation\ValidationException;
+use Log;
+use Saritasa\Laravel\Validation\GenericRuleSet;
+use Saritasa\Laravel\Validation\Rule;
+use Saritasa\LaravelRepositories\Contracts\IRepository;
+use Saritasa\LaravelRepositories\Exceptions\RepositoryException;
+use Throwable;
+use Validator;
 
 /**
  * Driver business-logic service.
  */
 class DriverService extends EntityService
 {
+    /**
+     * Company to driver assignments business-logic.
+     *
+     * @var DriversCardService
+     */
+    private $driversCardService;
+
+    /**
+     * Driver business-logic service.
+     *
+     * @param ConnectionInterface $connection Data storage connection
+     * @param IRepository $repository Handled entities storage
+     * @param DriversCardService $driversCardService Company to driver assignments business-logic
+     */
+    public function __construct(
+        ConnectionInterface $connection,
+        IRepository $repository,
+        DriversCardService $driversCardService
+    ) {
+        parent::__construct($connection, $repository);
+        $this->driversCardService = $driversCardService;
+    }
+
+    /**
+     * Returns validation rule to store or update driver.
+     *
+     * @param Driver $driver Driver to build rules for
+     *
+     * @return string[]|GenericRuleSet[]
+     */
+    protected function getDriverValidationRules(Driver $driver): array
+    {
+        return [
+            Driver::COMPANY_ID => Rule::required()->exists('companies', Company::ID),
+            Driver::FULL_NAME => Rule::required()
+                // Driver full name should be unique in company
+                ->unique('drivers', Driver::FULL_NAME, function (Unique $rule) use ($driver) {
+                    if ($driver->exists) {
+                        $rule->whereNot(Driver::ID, $driver->id);
+                    }
+
+                    return $rule->whereNull(Driver::DELETED_AT);
+                }),
+            Driver::CARD_ID => Rule::nullable()
+                // Card should exists and be of the driver card type
+                ->exists('cards', Card::ID, function (Exists $rule) use ($driver) {
+                    $rule->where(Card::CARD_TYPE_ID, CardTypesIdentifiers::DRIVER);
+
+                    return $rule->whereNull(Card::DELETED_AT);
+                })
+                // Card should be used only once
+                ->unique('drivers', Driver::CARD_ID, function (Unique $rule) use ($driver) {
+                    if ($driver->exists) {
+                        $rule->whereNot(Driver::ID, $driver->id);
+                    }
+
+                    return $rule->whereNull(Driver::DELETED_AT);
+                }),
+            Driver::BUS_ID => Rule::nullable()
+                // Bus should be in the same company with driver
+                ->exists('buses', Bus::ID, function (Exists $rule) use ($driver) {
+                    $rule->where(Bus::COMPANY_ID, $driver->company_id);
+
+                    return $rule->whereNull(Bus::DELETED_AT);
+                }),
+        ];
+    }
+
+    /**
+     * Stores new driver.
+     *
+     * @param DriverData $driverData Driver details to create
+     *
+     * @return Driver
+     *
+     * @throws RepositoryException
+     * @throws ValidationException
+     * @throws Throwable
+     */
+    public function store(DriverData $driverData): Driver
+    {
+        Log::debug("Create driver with name [{$driverData->full_name}] attempt");
+
+        $driver = new Driver($driverData->toArray());
+
+        Validator::validate($driverData->toArray(), $this->getDriverValidationRules($driver));
+
+        $this->handleTransaction(function () use ($driver): void {
+            $this->getRepository()->create($driver);
+
+            if ($driver->card_id) {
+                $this->driversCardService->openPeriod($driver->card, $driver);
+            }
+        });
+
+        Log::debug("Driver [{$driver->id}] created");
+
+        return $driver;
+    }
+
+    /**
+     * Updates driver details.
+     *
+     * @param Driver $driver Driver to update
+     * @param DriverData $driverData Driver new details
+     *
+     * @return Driver
+     *
+     * @throws RepositoryException
+     * @throws ValidationException
+     * @throws Throwable
+     */
+    public function update(Driver $driver, DriverData $driverData): Driver
+    {
+        Log::debug("Update driver [{$driver->id}] attempt");
+
+        Validator::validate($driverData->toArray(), $this->getDriverValidationRules($driver));
+
+        $cardChanged = $driver->card_id !== $driverData->card_id;
+        $cardWasAssigned = $driver->card_id;
+        $cardAssigned = $driverData->card_id;
+
+        if ($driver->company_id !== $driverData->company_id) {
+            Log::debug("Driver [{$driver->id}] cannot be reassigned to another company");
+
+            throw new DriverReassignException($driver);
+        }
+
+        $this->handleTransaction(function () use (
+            $cardWasAssigned,
+            $cardAssigned,
+            $driverData,
+            $cardChanged,
+            $driver
+        ): void {
+            $date = Carbon::now();
+
+            if ($cardWasAssigned && $cardChanged) {
+                // Close period for old company
+                $driversCard = $this->driversCardService->getForDriver($driver, $date);
+                if ($driversCard->card_id !== $driver->card_id) {
+                    throw new UnexpectedCardForDriverException($driversCard, $driver->card);
+                }
+                $this->driversCardService->closePeriod($driversCard, $date);
+            }
+
+            $newAttributes = $driverData->toArray();
+            $driver->fill($newAttributes);
+            $this->getRepository()->save($driver);
+
+            if ($cardChanged && $cardAssigned) {
+                // Open period for new company
+                $this->driversCardService->openPeriod($driver->card, $driver, $date->copy()->addSecond());
+            }
+        });
+
+        Log::debug("Driver [{$driver->id}] updated");
+
+        return $driver;
+    }
+
+    /**
+     * Deletes driver.
+     *
+     * @param Driver $driver Driver to delete
+     *
+     * @throws RepositoryException
+     * @throws ValidationException
+     * @throws Throwable
+     */
+    public function destroy(Driver $driver): void
+    {
+        Log::debug("Delete driver [{$driver->id}] attempt");
+
+        if ($driver->card_id) {
+            Log::debug("Driver [{$driver->id}] has related records. Can't delete");
+
+            throw new DriverDeletionException($driver);
+        }
+
+        $this->handleTransaction(function () use ($driver): void {
+            $driversCard = $this->driversCardService->getForDriver($driver);
+            if ($driversCard->card_id !== $driver->card_id) {
+                throw new UnexpectedCardForDriverException($driversCard, $driver->card);
+            }
+            $this->driversCardService->closePeriod($driversCard);
+
+            $this->getRepository()->delete($driver);
+        });
+
+        Log::debug("Driver [{$driver->id}] deleted");
+    }
 }
--- app/Domain/Services/DriversCardService.php
+++ app/Domain/Services/DriversCardService.php
@@ -2,11 +2,166 @@
 
 namespace App\Domain\Services;
 
+use App\Domain\Dto\DriversCardData;
+use App\Domain\Exceptions\Constraint\DriverCardExistsException;
+use App\Domain\Exceptions\Integrity\TooManyDriverCardsException;
 use App\Extensions\EntityService;
+use App\Models\Card;
+use App\Models\Driver;
+use App\Models\DriversCard;
+use Carbon\Carbon;
+use Illuminate\Validation\ValidationException;
+use Log;
+use Saritasa\Laravel\Validation\DateRuleSet;
+use Saritasa\Laravel\Validation\GenericRuleSet;
+use Saritasa\Laravel\Validation\Rule;
+use Saritasa\Laravel\Validation\RuleSet;
+use Saritasa\LaravelRepositories\Exceptions\RepositoryException;
+use Validator;
 
 /**
- * DriversCard business-logic service.
+ * Cards to drivers assignments business-logic service.
  */
 class DriversCardService extends EntityService
 {
+    /**
+     * Returns validation rule to store or update card to driver assignment.
+     *
+     * @param DriversCard $driversCard Card to driver assignment to build rules for
+     *
+     * @return string[]|GenericRuleSet[]
+     */
+    protected function getDriverValidationRules(DriversCard $driversCard): array
+    {
+        return [
+            DriversCard::ACTIVE_FROM => Rule::required()->date()
+                ->when($driversCard->active_to, function (RuleSet $rule) {
+                    /**
+                     * Date rules set.
+                     *
+                     * @var DateRuleSet $rule
+                     */
+                    return $rule->before(DriversCard::ACTIVE_TO);
+                }),
+            DriversCard::ACTIVE_TO => Rule::nullable()->date()
+                ->when($driversCard->active_to, function (RuleSet $rule) {
+                    /**
+                     * Date rules set.
+                     *
+                     * @var DateRuleSet $rule
+                     */
+                    return $rule->after(DriversCard::ACTIVE_FROM);
+                }),
+            DriversCard::CARD_ID => Rule::required()->exists('cards', Card::ID),
+            DriversCard::DRIVER_ID => Rule::required()->exists('drivers', Driver::ID),
+        ];
+    }
+
+    /**
+     * Opens new card to driver assignment period.
+     *
+     * @param Card $card Card to assign driver to
+     * @param Driver $driver Driver to assign card to
+     * @param Carbon|null $activeFrom Start date of card to driver assignment period
+     *
+     * @return DriversCard
+     *
+     * @throws RepositoryException
+     * @throws ValidationException
+     * @throws TooManyDriverCardsException
+     */
+    public function openPeriod(Card $card, Driver $driver, ?Carbon $activeFrom = null): DriversCard
+    {
+        Log::debug("Create card [{$card->id}] to driver [{$driver->id}] assign attempt");
+
+        $activeFrom = $activeFrom ?? Carbon::now();
+
+        $cardDriverData = new DriversCardData([
+            DriversCardData::ACTIVE_FROM => $activeFrom,
+            DriversCardData::CARD_ID => $driver->card_id,
+            DriversCardData::DRIVER_ID => $driver->id,
+        ]);
+
+        $driversCard = new DriversCard($cardDriverData->toArray());
+
+        Validator::validate($cardDriverData->toArray(), $this->getDriverValidationRules($driversCard));
+
+        $cardDriver = $this->getForDriver($driver, $activeFrom);
+
+        if ($cardDriver) {
+            throw new DriverCardExistsException($cardDriver);
+        }
+
+        $this->getRepository()->create($driversCard);
+
+        Log::debug("Card to driver assignment [{$driversCard->id}] created");
+
+        return $driversCard;
+    }
+
+    /**
+     * Closes driver to card assignment period.
+     *
+     * @param DriversCard $driversCard Card to driver assignment period to close
+     * @param Carbon|null $activeTo Date of period at which period should be closed
+     *
+     * @return DriversCard
+     *
+     * @throws RepositoryException
+     * @throws ValidationException
+     */
+    public function closePeriod(DriversCard $driversCard, ?Carbon $activeTo = null): DriversCard
+    {
+        Log::debug("Close card to driver assignment period [{$driversCard->id}] attempt");
+
+        $driversCard->active_to = $activeTo ?? Carbon::now();
+
+        Validator::validate($driversCard->toArray(), $this->getDriverValidationRules($driversCard));
+
+        $this->getRepository()->save($driversCard);
+
+        Log::debug("Card to driver assignment [{$driversCard->id}] period closed");
+
+        return $driversCard;
+    }
+
+    /**
+     * Returns card to driver assignment that was active at passed date.
+     *
+     * @param Driver $driver Driver to retrieve assignment for
+     * @param Carbon|null $date Date to find tariff period
+     *
+     * @return DriversCard|null
+     *
+     * @throws TooManyDriverCardsException
+     */
+    public function getForDriver(Driver $driver, ?Carbon $date = null): ?DriversCard
+    {
+        $date = $date ?? Carbon::now();
+
+        $cardDrivers = $this->getRepository()->getWith(
+            [],
+            [],
+            [
+                [DriversCard::DRIVER_ID, $driver->getKey()],
+                [DriversCard::ACTIVE_FROM, '<=', $date],
+                [
+                    [
+                        [DriversCard::ACTIVE_TO, '=', null, 'or'],
+                        [DriversCard::ACTIVE_TO, '>=', $date, 'or'],
+                    ],
+                ],
+            ]
+        );
+
+        if ($cardDrivers->count() > 1) {
+            throw new TooManyDriverCardsException($date, $driver, $cardDrivers);
+        }
+
+        if ($cardDrivers->count() === 1) {
+            return $cardDrivers->first();
+        }
+
+        return null;
+    }
 }
