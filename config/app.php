80511bb6589204b491908c426f5bbbff44ea1d8a
Vladimir Berdnik <vladimir.a.berdnik@gmail.com>
2018-11-12 21:24:15 +0600

    Remove deleted packages usage

--- config/app.php
+++ config/app.php
@@ -205,8 +205,6 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class,
         'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
-
-        'Debugbar' => Barryvdh\Debugbar\Facade::class,
     ],
 
 ];
