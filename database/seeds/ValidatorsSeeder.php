726635476ca79cb8a59ea95adc25c81132125561
Vladimir Berdnik <vladimir.a.berdnik@gmail.com>
2018-11-13 00:27:06 +0600

    Improve validators seeder, fill activity period records

--- database/seeds/ValidatorsSeeder.php
+++ database/seeds/ValidatorsSeeder.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Models\Bus;
+use App\Models\BusesValidator;
 use App\Models\Validator;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class ValidatorsSeeder extends Seeder
@@ -20,6 +22,11 @@ class ValidatorsSeeder extends Seeder
                     $busToAssign = $buses->pop();
                     $validator->bus_id = $busToAssign->getKey();
                     $validator->save();
+                    BusesValidator::query()->create([
+                        BusesValidator::BUS_ID => $validator->bus_id,
+                        BusesValidator::VALIDATOR_ID => $validator->id,
+                        BusesValidator::ACTIVE_FROM => Carbon::now(),
+                    ]);
                 }
             });
     }
