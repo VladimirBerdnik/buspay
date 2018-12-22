3b50e4d0db43bb00388568d771c841f02f14ffaa
Vladimir Berdnik <vladimir.a.berdnik@gmail.com>
2018-12-22 15:29:39 +0600

    Add table cell component with ability to check intentions

---
+++ resources/assets/app/views/tables/components/ActionCell.vue
@@ -0,0 +1,40 @@
+<template>
+  <td :class="classes"
+      @click="intentionAllowed ? $emit('activate') : null"
+  >
+    <slot/>
+  </td>
+</template>
+<script>
+export default {
+  name:  'ActionCell',
+  props: {
+    itemType: {
+      type:    String,
+      default: null,
+    },
+    intention: {
+      type:    String,
+      default: null,
+    },
+  },
+  computed: {
+    /**
+     * Whether action of component allowed or not.
+     *
+     * @return {boolean}
+     */
+    intentionAllowed() {
+      return this.policies.actionAllowed(this.itemType, this.intention);
+    },
+    /**
+     * Style classes of component.
+     *
+     * @return {string}
+     */
+    classes() {
+      return this.intentionAllowed ? 'action-cell' : '';
+    },
+  },
+};
+</script>
