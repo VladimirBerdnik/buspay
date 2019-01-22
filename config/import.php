e7b8e425983f644013beb1861c54a3a2351f2fc6
Vladimir Berdnik <vladimir.a.berdnik@gmail.com>
2019-01-23 00:04:13 +0600

    Add replenishment import configuration

--- config/import.php
+++ config/import.php
@@ -11,4 +11,11 @@ return [
     'validator' => [
         'importChunkSize' => 50,
     ],
+    'replenishment' => [
+        'importChunkSize' => 50,
+        // Replenishment records within given days interval will be checked for presence in local and external storage
+        'synchronisationDaysInterval' => 14,
+        // Suspicious replenishment amount. If replenishment will be greater than given value then notice will be logged
+        'suspiciousAmountLimit' => 10000,
+    ],
 ];
