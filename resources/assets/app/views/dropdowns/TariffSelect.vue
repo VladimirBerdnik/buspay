73b73a46ac68c864dd0f777a4472d89613919480
Vladimir Berdnik <vladimir.a.berdnik@gmail.com>
2019-02-05 22:12:37 +0600

    Add tariff and validator dropdowns components

---
+++ resources/assets/app/views/dropdowns/TariffSelect.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-autocomplete v-model="itemId"
+                  :items="items"
+                  :label="$t('tariff.name')"
+                  :clearable="!readonly && clearable"
+                  :readonly="readonly"
+                  :error-messages="errorMessages"
+                  :item-text="itemText"
+                  :item-value="itemValue"
+                  :loading="loading"
+                  :append-outer-icon="!readonly ? 'cached' : null"
+                  persistent-hint
+                  @click:append-outer="reloadItems"
+  />
+</template>
+
+<script>
+import TariffsService from '../../services/TariffsService';
+import FilterableDropdownMixin from '../../mixins/FilterableDropdownMixin';
+
+export default {
+  name:   'TariffSelect',
+  mixins: [FilterableDropdownMixin],
+  data() {
+    return {
+      itemText: 'serial_number',
+      service:  TariffsService,
+    };
+  },
+};
+</script>
---
+++ resources/assets/app/views/dropdowns/ValidatorSelect.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-autocomplete v-model="itemId"
+                  :items="items"
+                  :label="$t('validator.name')"
+                  :clearable="!readonly && clearable"
+                  :readonly="readonly"
+                  :error-messages="errorMessages"
+                  :item-text="itemText"
+                  :item-value="itemValue"
+                  :loading="loading"
+                  :append-outer-icon="!readonly ? 'cached' : null"
+                  persistent-hint
+                  @click:append-outer="reloadItems"
+  />
+</template>
+
+<script>
+import ValidatorsService from '../../services/ValidatorsService';
+import FilterableDropdownMixin from '../../mixins/FilterableDropdownMixin';
+
+export default {
+  name:   'ValidatorSelect',
+  mixins: [FilterableDropdownMixin],
+  data() {
+    return {
+      itemText: 'serial_number',
+      service:  ValidatorsService,
+    };
+  },
+};
+</script>
